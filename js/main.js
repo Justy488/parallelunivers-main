0713e99e6334187f78c7d91aed59a3e121630798
Justina Cudjoe <jucu0001@stud.ek.dk>
2025-11-04 12:22:42 +0100

    tilføjet til infobox og tilføjet svg. samt udfyldningsskema

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ infobtnHotel.addEventListener("mouseup", displayInfo);
 function displayInfo() {
   console.log("test");
 }
+
+// infobox*/
+document.querySelector("#ufo").addEventListener("click", updateinfoboxname);
+
+function updateinfoboxname() {
+  console.log("updateinfoboxname");
+  document.querySelector(".info-text h2").textContent = "UFO";
+  document.querySelector(".info-text .placeholder").textContent = "";
+  document.querySelector("#efficiency").innerHTML =
+    "<h3>Underrubrik</h3><p>lorem</p>";
+  document.querySelector("#requirement").innerHTML =
+    "<h3>Underrubrik</h3><p></p>";
+}
